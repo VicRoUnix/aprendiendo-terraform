b1628b76274b665958420aafb4877a30c06d6ef9
ViRoUnix <141222282+VicRoUnix@users.noreply.github.com>
2026-02-11 19:19:31 +0100

    practicando

---
+++ introduccion-a-terraform/first-prac/variables.tf
@@ -0,0 +1,22 @@
+variable "student_name" {
+  description = "NOmbre del estudiante"
+  type        = string
+  default     = "DevOps Student"
+
+  validation {
+    condition     = length(var.student_name) > 2
+    error_message = "El nombre debe tener al menos 3 caracteres."
+  }
+}
+
+variable "devops_tools" {
+  description = "Herramientas DevOps que estamos aprendiendo"
+  type        = list(string)
+  default     = ["Docker", "Docker Compose", "Terraform", "GitHub Actions", "Kubernetes"]
+}
+
+variable "create_backup" {
+  description = "Crear archivo de respaldo"
+  type        = bool
+  default     = true
+}
